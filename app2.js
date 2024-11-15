f40c9a8490084fbbac29594c8698a0dcf2429056
David Lewis <demandgendave@gmail.com>
2024-11-15 00:07:26 -0800

    updates

--- app2.js
+++ app2.js
@@ -401,11 +401,10 @@ function renderEventsList(events) {
                 <div class="event-info">
                     <div class="event-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M22 16.32A11.5 11.5 0 0 1 10.5 5a11.5 11.5 0 0 1 11.5 11.32z"/>
-                            <path d="M22 16.32A11.5 11.5 0 0 0 10.5 5a11.5 11.5 0 0 0 11.5 11.32z"/>
-                            <path d="M10.5 5a11.5 11.5 0 0 0 0 23"/>
-                            <path d="M10.5 5a11.5 11.5 0 0 1 0 23"/>
-                            <path d="M2 16h20"/>
+                            <rect x="3" y="4" width="18" height="18" rx="2" ry="2"/>
+                            <line x1="16" y1="2" x2="16" y2="6"/>
+                            <line x1="8" y1="2" x2="8" y2="6"/>
+                            <line x1="3" y1="10" x2="21" y2="10"/>
                         </svg>
                     </div>
                     <div class="event-name">${event.title}</div>
@@ -620,7 +619,7 @@ function renderEventDetail(eventId, eventData) {
                             <input type="checkbox" 
                                 ${eventData.anonymous ? 'checked' : ''} 
                                 onchange="toggleAnonymous('${eventId}', this.checked)">
-                            Anonymous Responses
+                            Make Responses Anonymous
                         </label>
                     </div>
                 </div>
